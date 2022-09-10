3b75ff55ff103cabfff66055dde46c43127674fb
EC2 Default User <ec2-user@ip-172-31-95-39.ec2.internal>
2022-09-10 13:42:56 +0000

    Modified config.py for linux ec2 instance

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 #MySql
-DB_USER = 'shashi'
+DB_USER = 'admin'
 DB_HOST = 'localhost'
 DATABASE_NAME = 'assignment'
-DB_PASSWORD = '1234'
+DB_PASSWORD = 'Shashi@456'
 TABLE_NAME = 'file_processing_status'
 
-PROCESSING_FOLDER = 'D:\\mycode\\file_processing_assignment\\processing\\'
-PROCESSED_FOLDER = 'D:\\mycode\\file_processing_assignment\\processed\\'
-QUEUE_FOLDER = 'D:\\mycode\\file_processing_assignment\\queue\\'
+PROCESSING_FOLDER = '/home/ec2-user/FILE_PROCESSING_ASSIGNMENT/a_file_processing_task/processing/'
+PROCESSED_FOLDER = '/home/ec2-user/FILE_PROCESSING_ASSIGNMENT/a_file_processing_task/processed/'
+QUEUE_FOLDER = '/home/ec2-user/FILE_PROCESSING_ASSIGNMENT/a_file_processing_task/queue/'
