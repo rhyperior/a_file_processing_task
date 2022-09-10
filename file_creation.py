5c61ea317df69691ac068f2064a42e991f49159f
Shashi Shekhar <fantabulousshashi@gamil.com>
2022-09-10 13:01:53 +0530

    feat: Working solution

--- file_creation.py
+++ file_creation.py
@@ -37,7 +37,7 @@ async def move_files(origin_folder, destination_folder):
     except Exception as e:
         print("Exception in move_files_to_queue fn", str(e))
 
-async def update_file_details_to_db(query=None):
+def update_file_details_to_db(query=None):
     try:
         conn = connect(user=cfg.DB_USER, host=cfg.DB_HOST, database=cfg.DATABASE_NAME, password=cfg.DB_PASSWORD)
         print(conn) 
@@ -52,7 +52,7 @@ async def update_file_details_to_db(query=None):
     except Exception as e:
         print("error in update_file_details_to_db fn", str(e))
 
-async def query_builder(filename_list, action='insert'):
+def query_builder(filename_list, action='insert'):
     try:
         if action == 'insert':
             query = f'INSERT INTO {cfg.TABLE_NAME} (filename, status, inserted_at) VALUES '
@@ -62,11 +62,11 @@ async def query_builder(filename_list, action='insert'):
             values = values[:-1] + ';'
             query = query + values
         else:
-            query = f'UPDATE {cfg.TABLE_NAME} SET status=1, processed_at=now() WHERE filename in '
+            query = f'UPDATE {cfg.TABLE_NAME} SET status=1, processed_at=now() WHERE filename in ('
             values = ''
             for filename in filename_list:
-                values = values + f"('{filename}',"
-            values = values[:-1] + ';'
+                values = values + f"'{filename}',"
+            values = values[:-1] + ') ;'
             query = query + values
         return query
 
@@ -77,26 +77,36 @@ async def process_queued_files():
     try:
         text_files = [f for f in listdir(cfg.QUEUE_FOLDER) if isfile(join(cfg.QUEUE_FOLDER, f)) and '.txt' in f]
         insert_query = query_builder(text_files)
-        print(insert_query)
+        # print(insert_query)
         update_file_details_to_db(query=insert_query)
-        move_files(cfg.QUEUE_FOLDER, cfg.PROCESSED_FOLDER)
+        task_move_files = asyncio.create_task(move_files(cfg.QUEUE_FOLDER, cfg.PROCESSED_FOLDER))
+        await task_move_files
         print('updated details in db and moved files to processed')
     except Exception as e:
         print("error in process_queued_files fn", str(e))
 
 async def finish_processing_files():
     try:
-        text_files = [f for f in listdir(cfg.QUEUE_FOLDER) if isfile(join(cfg.QUEUE_FOLDER, f)) and '.txt' in f]
+        text_files = [f for f in listdir(cfg.PROCESSED_FOLDER) if isfile(join(cfg.PROCESSED_FOLDER, f)) and '.txt' in f]
         update_query = query_builder(text_files, action='update')
+        print(update_query)
         update_file_details_to_db(query=update_query)
         print('finished processing')
     except Exception as e:
         print('error in finish_processing_files fn', str(e))
 
 async def main():
-    pass
+    task_1 = asyncio.create_task(create_new_textfile())
+    await task_1
+    task_2 = asyncio.create_task(move_files(cfg.PROCESSING_FOLDER, cfg.QUEUE_FOLDER))
+    task_3 = asyncio.create_task(process_queued_files())
+    await task_2
+    await task_3
+    task_4 = asyncio.create_task(finish_processing_files())
+    await task_4
 
 if __name__=="__main__":
-    pass
+    while(True):
+        asyncio.run(main())
 
     
