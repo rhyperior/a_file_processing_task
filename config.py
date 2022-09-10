70558b11bf8ef95834dc9313db62d90fd13018e9
Shashi Shekhar <fantabulousshashi@gamil.com>
2022-09-10 12:17:16 +0530

    Initial Commit.

---
+++ config.py
@@ -0,0 +1,10 @@
+#MySql
+DB_USER = 'shashi'
+DB_HOST = 'localhost'
+DATABASE_NAME = 'assignment'
+DB_PASSWORD = '1234'
+TABLE_NAME = 'file_processing_status'
+
+PROCESSING_FOLDER = 'D:\\mycode\\file_processing_assignment\\processing\\'
+PROCESSED_FOLDER = 'D:\\mycode\\file_processing_assignment\\processed\\'
+QUEUE_FOLDER = 'D:\\mycode\\file_processing_assignment\\queue\\'
---
+++ file_creation.py
@@ -0,0 +1,102 @@
+from os import listdir, system
+from os.path import isfile, join
+import asyncio
+import time
+import random
+import string
+from datetime import datetime
+from mysql.connector import connect
+
+
+import config as cfg
+
+PROCESSING_FOLDER = cfg.PROCESSING_FOLDER
+PROCESSED_FOLDER = cfg.PROCESSED_FOLDER
+QUEUE_FOLDER = cfg.QUEUE_FOLDER
+
+
+async def create_new_textfile(): 
+    try:
+        i = 5
+        while(i > 0):
+            text = "".join( [random.choice(string.ascii_letters) for i in range(50)])
+            filename = str(datetime.now().strftime('%Y-%d-%m__%H-%M-%S-%f')) + '__' +text[:10]+'.txt'
+            print(filename)
+            with open(PROCESSING_FOLDER+filename, 'w') as f:
+                f.write(text)
+            print('Just created a new text file.')
+            time.sleep(0.9) 
+            i -= 1
+        await asyncio.sleep(1)
+    except Exception as e:
+        print("error in create_new_textfile fn", str(e))
+
+async def move_files(origin_folder, destination_folder):
+    try:
+        system(f"MOVE {origin_folder}*txt {destination_folder}")
+    except Exception as e:
+        print("Exception in move_files_to_queue fn", str(e))
+
+async def update_file_details_to_db(query=None):
+    try:
+        conn = connect(user=cfg.DB_USER, host=cfg.DB_HOST, database=cfg.DATABASE_NAME, password=cfg.DB_PASSWORD)
+        print(conn) 
+
+        with conn.cursor() as cur:
+            cur.execute(query)
+
+        conn.commit()
+        # print('sleeping for 10 seconds..')
+        # time.sleep(3)
+        conn.close()
+    except Exception as e:
+        print("error in update_file_details_to_db fn", str(e))
+
+async def query_builder(filename_list, action='insert'):
+    try:
+        if action == 'insert':
+            query = f'INSERT INTO {cfg.TABLE_NAME} (filename, status, inserted_at) VALUES '
+            values = ''
+            for filename in filename_list:
+                values = values + f"('{filename}', 0, now()) ,"
+            values = values[:-1] + ';'
+            query = query + values
+        else:
+            query = f'UPDATE {cfg.TABLE_NAME} SET status=1, processed_at=now() WHERE filename in '
+            values = ''
+            for filename in filename_list:
+                values = values + f"('{filename}',"
+            values = values[:-1] + ';'
+            query = query + values
+        return query
+
+    except Exception as e:
+        print("error in query_builder fn", str(e))
+
+async def process_queued_files():
+    try:
+        text_files = [f for f in listdir(cfg.QUEUE_FOLDER) if isfile(join(cfg.QUEUE_FOLDER, f)) and '.txt' in f]
+        insert_query = query_builder(text_files)
+        print(insert_query)
+        update_file_details_to_db(query=insert_query)
+        move_files(cfg.QUEUE_FOLDER, cfg.PROCESSED_FOLDER)
+        print('updated details in db and moved files to processed')
+    except Exception as e:
+        print("error in process_queued_files fn", str(e))
+
+async def finish_processing_files():
+    try:
+        text_files = [f for f in listdir(cfg.QUEUE_FOLDER) if isfile(join(cfg.QUEUE_FOLDER, f)) and '.txt' in f]
+        update_query = query_builder(text_files, action='update')
+        update_file_details_to_db(query=update_query)
+        print('finished processing')
+    except Exception as e:
+        print('error in finish_processing_files fn', str(e))
+
+async def main():
+    pass
+
+if __name__=="__main__":
+    pass
+
+    
